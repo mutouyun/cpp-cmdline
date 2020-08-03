81dbc0e16993615fddbfe15bd58007969f29a912
mutouyun <orz@orzz.org>
2020-08-03 23:04:21 +0800

    优化代码（TBD）

--- include/cmdline.h
+++ include/cmdline.h
@@ -10,11 +10,146 @@
 #include <functional>
 #include <utility>
 #include <iostream>
+#include <cstddef>
+#include <cassert>
+#include <cstring>
+#include <algorithm>
 
 namespace cmdline {
 
+class str_view {
+
+    char const * str_  = nullptr;
+    std::size_t  size_ = 0;
+
+public:
+    str_view() noexcept = default;
+
+    str_view(std::string const & s) noexcept
+        : str_ (s.c_str())
+        , size_(s.size ()) {
+    }
+
+    str_view(char const * str, std::size_t size) noexcept
+        : str_ (str )
+        , size_((str == nullptr) ? 0 : size) {
+    }
+
+    str_view(char const * str)
+        : str_ (str)
+        , size_((str == nullptr) ? 0 : std::string::npos) {
+    }
+
+    template <std::size_t N>
+    str_view(char const (& str)[N]) noexcept
+        : str_ (str )
+        , size_(N - 1) {
+    }
+
+    void swap(str_view & rhs) noexcept {
+        std::swap(str_ , rhs.str_ );
+        std::swap(size_, rhs.size_);
+    }
+
+    char const * data() const noexcept {
+        return str_;
+    }
+
+    std::size_t size() const noexcept {
+        return size_;
+    }
+
+    std::size_t length() const {
+        return (size() == std::string::npos) ? (assert(str_ != nullptr), std::strlen(str_)) : size();
+    }
+
+    bool empty() const noexcept {
+        return (data() == nullptr) || (size() == 0) || (data()[0] == '\0');
+    }
+
+    std::size_t find_first_of(char const c) const noexcept {
+        if (empty()) return std::string::npos;
+        for (std::size_t i = 0; i < size(); ++i) {
+            if (data()[i] == c) {
+                return i;
+            }
+            else if (data()[i] == '\0') {
+                return std::string::npos;
+            }
+            else continue;
+        }
+        return std::string::npos;
+    }
+
+    std::size_t find_last_of(char const c) const {
+        if (empty()) return std::string::npos;
+        std::size_t sz = length();
+        for (std::size_t i = sz - 1; i > 0; --i) {
+            if (data()[i] == c) {
+                return i;
+            }
+            else continue;
+        }
+        return (data()[0] == c) ? 0 : std::string::npos;
+    }
+
+    str_view substr(std::size_t off, std::size_t count = std::string::npos) const noexcept {
+        if (empty()) return {};
+        if (count == 0) return {};
+        std::size_t sz = length();
+        if (off >= sz) return {};
+        return { data() + off, std::min<std::size_t>(count, sz - off) };
+    }
+
+    char & operator[](std::size_t i) noexcept {
+        return const_cast<char *>(data())[i];
+    }
+
+    char const operator[](std::size_t i) const noexcept {
+        return (*const_cast<str_view *>(this))[i];
+    }
+
+    friend bool operator==(str_view const & lhs, str_view const & rhs) noexcept {
+        if ((lhs.data() == rhs.data()) && (lhs.size() == rhs.size())) {
+            return true;
+        }
+        if (lhs.empty() && rhs.empty()) return true;
+        if (lhs.empty() || rhs.empty()) return false;
+
+        str_view const * longer;
+        std::size_t e;
+        if (lhs.size() < rhs.size()) {
+            longer = &rhs;
+            e = lhs.size();
+        }
+        else if (lhs.size() > rhs.size()) {
+            longer = &lhs;
+            e = rhs.size();
+        }
+        // lhs.size() == rhs.size()
+        else return std::memcmp(lhs.data(), rhs.data(), lhs.size()) == 0;
+
+        for (std::size_t i = 0; i < e; ++i) {
+            if (lhs[i] != rhs[i]) return false;
+        }
+        assert(longer != nullptr);
+        return (*longer)[e] == '\0';
+    }
+
+    friend bool operator!=(str_view const & lhs, str_view const & rhs) noexcept {
+        return !(lhs == rhs);
+    }
+
+    friend std::ostream & operator<<(std::ostream & o, str_view const & sv) {
+        for (std::size_t i = 0; (i < sv.size()) && (sv[i] != '\0'); ++i) {
+            o << sv[i];
+        }
+        return o;
+    }
+};
+
 class parser;
-using handle_t = void(parser const &, std::string const &);
+using handle_t = void(parser const &, str_view const &);
 
 struct option {
     char const * sname_;
@@ -25,20 +160,20 @@ struct option {
     std::function<handle_t> handle_;
 };
 
-using options = std::deque<option>;
+using options_t = std::deque<option>;
 
 class parser {
 
-    options necessary_, optional_;
+    options_t necessary_, optional_;
     std::function<handle_t> usage_;
-    std::string path_;
+    str_view path_;
 
 public:
-    options &       necessary(void)       { return necessary_; }
-    options const & necessary(void) const { return necessary_; }
+    options_t &       necessary(void)       { return necessary_; }
+    options_t const & necessary(void) const { return necessary_; }
 
-    options &       optional(void)       { return optional_; }
-    options const & optional(void) const { return optional_; }
+    options_t &       optional(void)       { return optional_; }
+    options_t const & optional(void) const { return optional_; }
 
     std::function<handle_t> &       usage(void)       { return usage_; }
     std::function<handle_t> const & usage(void) const { return usage_; }
@@ -49,9 +184,9 @@ public:
             out << "Must has at least one argument (the path of current program)." << std::endl;
             return;
         }
-        size_t slash = path_.find_last_of('\\');
+        std::size_t slash = path_.find_last_of('\\');
         if (slash == std::string::npos) slash = path_.find_last_of('/');
-        std::string name = path_.substr(slash + 1);
+        str_view name = path_.substr(slash + 1);
         if (usage_) {
             usage_(*this, name);
         }
@@ -82,12 +217,12 @@ public:
         this->print_usage(std::cout);
     }
 
-    void push(options && opts) {
+    void push(options_t && opts) {
         for (auto && o : opts) {
             if (o.lname_ == nullptr) {
                 continue;
             }
-            options * list = &(o.necessary_ ? necessary_ : optional_);
+            options_t * list = &(o.necessary_ ? necessary_ : optional_);
             list->emplace_back(std::move(o));
         }
     }
@@ -98,26 +233,28 @@ public:
     }
 
     template <typename T>
-    void exec(T && out, int argc, char const * const argv[]) {
+    int exec(T && out, int argc, char const * const argv[]) {
         if (argc >= 1) path_ = argv[0];
-        if (argc <= 1) this->print_usage(std::forward<T>(out));
+        if (argc <= 1) {
+            this->print_usage(std::forward<T>(out));
+        }
         else {
             struct ST_opt {
                 std::function<handle_t> const & hd_;
-                std::string cm_;
+                str_view cm_;
             };
             std::deque<ST_opt> exec_list;
-            size_t c_nec = 0;
+            std::size_t c_nec = 0;
             for (int i = 1; i < argc; ++i) {
-                std::string a = argv[i];
-                size_t c = a.find_first_of('=');
-                std::string o = a.substr(0, c);
-                auto foreach = [&](auto const & cc, auto && fr) {
-                    for (auto it = cc.begin(); it != cc.end(); ++it) {
-                        if (((it->sname_ != nullptr) && (o == it->sname_)) || (o == it->lname_)) {
+                str_view    a = argv[i];
+                std::size_t c = a.find_first_of('=');
+                str_view    o = a.substr(0, c);
+                auto foreach = [&](options_t const & cc, auto && fr) {
+                    for (option const & it : cc) {
+                        if (((it.sname_ != nullptr) && (o == it.sname_)) || (o == it.lname_)) {
                             exec_list.emplace_back(ST_opt {
-                                it->handle_,
-                                c == std::string::npos ? it->default_ : a.substr(c + 1)
+                                it.handle_,
+                                (c == std::string::npos) ? it.default_ : a.substr(c + 1)
                             });
                             fr();
                         }
@@ -126,15 +263,18 @@ public:
                 foreach(necessary_, [&c_nec] { ++c_nec; });
                 foreach(optional_ , [] {});
             }
-            if (c_nec != necessary_.size() || exec_list.empty()) this->print_usage(std::forward<T>(out));
-            else for (auto & e : exec_list) {
+            if ((c_nec != necessary_.size()) || exec_list.empty()) {
+                this->print_usage(std::forward<T>(out));
+            }
+            else for (ST_opt & e : exec_list) {
                 e.hd_(*this, e.cm_);
             }
         }
+        return 0;
     }
 
-    void exec(int argc, char const * const argv[]) {
-        this->exec(std::cout, argc, argv);
+    int exec(int argc, char const * const argv[]) {
+        return this->exec(std::cout, argc, argv);
     }
 };
 
--- ut/ut-cmdline.cpp
+++ ut/ut-cmdline.cpp
@@ -1,26 +1,27 @@
+#include <cstdlib>
+
 #include "cmdline.h"
+
+#define ASSERT_EQ(A, B) ((void)(((A) == (B)) || (std::abort(), 1)))
 
 int main(int argc, char * argv[]) {
     cmdline::parser a;
-    a.push(cmdline::options
-        {
+    a.push({
             {
                 "-h", "--help", "Print usage.", false, "",
                 [](auto & a, auto &) {
                     a.print_usage();
                 }
-            },
-            {
+            }, {
                 nullptr, "--test", "You must use this option.", true, "",
                 [](auto &, auto &) { /*Do Nothing.*/ }
-            },
-            {
+            }, {
                 "-o", "--output", "Print text.", true, "Hello World!",
                 [](auto &, auto & str) {
                     std::cout << str << std::endl;
                 }
             }
         });
-    a.exec(argc, argv);
+    ASSERT_EQ(a.exec(argc, argv), 0);
     return 0;
 }
